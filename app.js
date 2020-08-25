16ede42b9c0fac616810a36efffaf9d1f8eec46a
jfcarocota <jfcarocota@gmail.com>
2020-08-25 10:45:41 -0700

    abilites and effecs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const pokemon = {
     sprite: document.getElementById('pokemon-image'),
-    name: document.getElementById('pokemon-name')
+    name: document.getElementById('pokemon-name'),
+    types: document.getElementById('pokemon-types'),
+    abilities: document.getElementById('pokemon-abilities')
 }
 
 
@@ -9,11 +11,67 @@ const GetPokemon = async ()=>{
 
     const data = await fetch(url);
     const dataJson = await data.json();
-    
-    console.log(await dataJson);
 
-    pokemon.sprite.src = await dataJson.sprites.front_default;
-    pokemon.name.innerHTML = await `Name: ${dataJson.name}`;
+    const {sprites, name, types, abilities} = dataJson;
+
+    types.forEach(element => {
+        const {type} = element;
+        //console.log(type.name);
+        pokemon.types.innerHTML += `<li>${type.name}</li>`;
+    });
+
+    abilities.forEach(element => {
+        const{ability} = element;
+        //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+        //console.log(ability.url);
+        GetAbilityInfo(ability.url,  ability.name);
+
+        //console.log(pokemonAbilitesInfo);
+    });
+    
+    //console.log(await abilities);
+
+    pokemon.sprite.src = await sprites.front_default;
+    pokemon.name.innerHTML = await `Name: ${name}`;
+}
+
+const GetAbilityInfo= async (url, abilityName)=>{
+
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    const{effect_entries} = dataJson;
+
+    let effectsList = '';
+
+    effect_entries.forEach(element => {
+
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            effectsList += `<li>${effect}</li>`;
+        }
+    });
+
+    pokemon.abilities.innerHTML += 
+    `<li>
+        ${abilityName}
+        <div>effect</div>
+        <ul>
+            ${effectsList}
+        </ul>
+    </li>`;
+
+    /*const abilitiesPokemon = effect_entries.map( element =>{
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            return effect;
+        }
+    });*/
+
+    //console.log(abilitiesPokemon);
 }
 
 GetPokemon();
